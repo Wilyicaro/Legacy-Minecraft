2d92728c2b9160b8aba8e5b8a39e9f2bce5d6992
creepereater201 <258679852+creepereater201@users.noreply.github.com>
2026-03-17 17:08:34 +0000

    LCE Falling Block Parity

---
+++ src/main/java/wily/legacy/mixin/base/FallingBlockEntityMixin.java
@@ -0,0 +1,45 @@
+package wily.legacy.mixin.base;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.item.FallingBlockEntity;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.Vec3;
+import net.minecraft.world.phys.AABB;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import wily.legacy.init.LegacyGameRules;
+
+@Mixin(FallingBlockEntity.class)
+public abstract class FallingBlockEntityMixin {
+    private static final AABB fallingBlockDetectBounding = new AABB(-50, -50, -50, 50, 50, 50);
+    @Shadow public int time;
+
+    @Inject(method = "fall", at = @At("HEAD"), cancellable = true)
+    private static void fall(Level level, BlockPos blockPos, BlockState blockState, CallbackInfoReturnable<FallingBlockEntity> cir) {
+        if (level instanceof ServerLevel serverLevel && serverLevel.getGameRules().getRule(LegacyGameRules.LCE_FALLING_BLOCK_LIMIT).get() > 0 && level.getEntitiesOfClass(FallingBlockEntity.class, fallingBlockDetectBounding.move(blockPos)).size() >= serverLevel.getGameRules().getRule(LegacyGameRules.LCE_FALLING_BLOCK_LIMIT).get()) {
+            cir.setReturnValue(null);
+        }
+    }
+
+    @Inject(method = "tick", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/item/FallingBlockEntity;onGround()Z"), cancellable = true)
+    private void tick(CallbackInfo ci) {
+        Entity entity = (Entity) (Object) this;
+        if (!entity.onGround()) return;
+        BlockPos blockPos = entity.blockPosition().below();
+        Level level = entity.level();
+        BlockState blockState = level.getBlockState(blockPos);
+        if (blockState.isAir() || Block.isFaceFull(blockState.getCollisionShape(level, blockPos), Direction.UP)) return;
+        time = 0;
+        entity.setDeltaMovement(Vec3.ZERO);
+        ci.cancel();
+    }
+}
