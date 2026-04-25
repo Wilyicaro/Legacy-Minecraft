05f5a9594d00048ec16d68da7560c4762bdfd90b
Wilyicaro <81383021+Wilyicaro@users.noreply.github.com>
2026-04-25 00:41:13 -0300

    reverted hardcoded Tutorial gamerule

--- src/main/java/wily/legacy/mobcaps/LegacyMobCaps.java
+++ src/main/java/wily/legacy/mobcaps/LegacyMobCaps.java
@@ -28,7 +28,7 @@ public final class LegacyMobCaps {
     }
 
     public static boolean isEnabled(ServerLevel level) {
-        return level.getGameRules().getBoolean(LegacyGameRules.LEGACY_MOBCAP_LIMITS) && !"Tutorial".equals(level.getServer().getWorldData().getLevelName());
+        return level.getGameRules().getBoolean(LegacyGameRules.LEGACY_MOBCAP_LIMITS);
     }
 
     public static void handleEntityAdded(Entity entity) {
