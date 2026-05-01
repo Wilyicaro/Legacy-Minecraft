26719dac4bcc915cbf0917a9d243d9e9e3fea6e3
Wilyicaro <icarokauanbomfim@hotmail.com>
2026-05-01 12:44:44 -0300

    fix(port): broken experience bar mixin and lightmap

--- src/main/java/wily/legacy/mixin/base/client/gui/ExperienceBarRendererMixin.java
+++ src/main/java/wily/legacy/mixin/base/client/gui/ExperienceBarRendererMixin.java
@@ -1,34 +1,38 @@
 package wily.legacy.mixin.base.client.gui;
 
 import net.minecraft.client.gui.Font;
-import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.GuiGraphicsExtractor;
 import net.minecraft.client.gui.contextualbar.ContextualBarRenderer;
 import net.minecraft.locale.Language;
 import net.minecraft.network.chat.FormattedText;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import wily.factoryapi.FactoryAPIClient;
 import wily.factoryapi.util.ColorUtil;
 import wily.legacy.client.CommonColor;
 import wily.legacy.client.LegacyOptions;
 import wily.legacy.util.client.LegacyRenderUtil;
 
-@Mixin(Gui.class)
-public class ExperienceBarRendererMixin {
+@Mixin(ContextualBarRenderer.class)
+public interface ExperienceBarRendererMixin {
 
-    @Redirect(method = /*? if forge || <26.1 {*//*"extractHotbarAndDecorations"*//*?} else {*/ "extractExperienceLevel" /*?}*/, at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/contextualbar/ContextualBarRenderer;extractExperienceLevel(Lnet/minecraft/client/gui/GuiGraphicsExtractor;Lnet/minecraft/client/gui/Font;I)V"))
-    private void renderExperienceLevel(GuiGraphicsExtractor GuiGraphicsExtractor, Font font, int i) {
+    @Inject(method = "extractExperienceLevel", at = @At("HEAD"), cancellable = true)
+    private static void extractExperienceLevel(GuiGraphicsExtractor guiGraphics, Font font, int i, CallbackInfo ci) {
+        ci.cancel();
+
         if (!LegacyRenderUtil.canDisplayHUD()) return;
 
-        GuiGraphicsExtractor.pose().pushMatrix();
-        GuiGraphicsExtractor.pose().translate(GuiGraphicsExtractor.guiWidth() / 2f, GuiGraphicsExtractor.guiHeight());
+        guiGraphics.pose().pushMatrix();
+        guiGraphics.pose().translate(guiGraphics.guiWidth() / 2f, guiGraphics.guiHeight());
         String exp = String.valueOf(i);
         int hudScale = LegacyOptions.hudSize.get();
         boolean hd = LegacyOptions.getUIMode().isHDOrLower();
-        GuiGraphicsExtractor.pose().translate(0, -36f);
-        if (!hd && hudScale != 1) GuiGraphicsExtractor.pose().scale(7 / 8f, 7 / 8f);
-        LegacyRenderUtil.drawOutlinedString(GuiGraphicsExtractor, font, Language.getInstance().getVisualOrder(FormattedText.of(exp)), -font.width(exp) / 2, -2, ColorUtil.withAlpha(CommonColor.EXPERIENCE_TEXT.get(), LegacyRenderUtil.getHUDOpacity()), ColorUtil.withAlpha(0xFF000000, LegacyRenderUtil.getHUDOpacity()), hd && hudScale == 3 || !hd && hudScale == 2 || hudScale == 1 ? 0.51f : 2 / 3f);
-        GuiGraphicsExtractor.pose().popMatrix();
+        guiGraphics.pose().translate(0, -36f);
+        if (!hd && hudScale != 1) guiGraphics.pose().scale(7 / 8f, 7 / 8f);
+        LegacyRenderUtil.drawOutlinedString(guiGraphics, font, Language.getInstance().getVisualOrder(FormattedText.of(exp)), -font.width(exp) / 2, -2, ColorUtil.withAlpha(CommonColor.EXPERIENCE_TEXT.get(), LegacyRenderUtil.getHUDOpacity()), ColorUtil.withAlpha(0xFF000000, LegacyRenderUtil.getHUDOpacity()), hd && hudScale == 3 || !hd && hudScale == 2 || hudScale == 1 ? 0.51f : 2 / 3f);
+        FactoryAPIClient.getProfiler().pop();
+        guiGraphics.pose().popMatrix();
     }
 }
