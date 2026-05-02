93c2dd0113ef1fc885b24bde2ac584032233eb53
creepereater201 <258679852+creepereater201@users.noreply.github.com>
2026-05-03 00:11:55 +0100

    fix: Display chat indicator option

--- src/main/java/wily/legacy/mixin/base/client/chat/ChatComponentDrawingMixin.java
+++ src/main/java/wily/legacy/mixin/base/client/chat/ChatComponentDrawingMixin.java
@@ -6,15 +6,27 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import wily.legacy.client.LegacyOptions;
 import wily.legacy.util.client.LegacyRenderUtil;
 
 @Mixin(targets = {"net.minecraft.client.gui.components.ChatComponent$DrawingBackgroundGraphicsAccess", "net.minecraft.client.gui.components.ChatComponent$DrawingFocusedGraphicsAccess"} )
 public class ChatComponentDrawingMixin {
 
-    @Inject(method = "handleTag", at = @At(value = "HEAD"))
+    @Inject(method = "handleTag", at = @At(value = "HEAD"), cancellable = true)
     private void handleTag(CallbackInfo ci, @Local(argsOnly = true, ordinal = 0) LocalIntRef x0, @Local(argsOnly = true, ordinal = 2) LocalIntRef x1) {
+        if (!LegacyOptions.displayChatIndicators.get()) {
+            ci.cancel();
+            return;
+        }
+
         int safeZone = Math.round(LegacyRenderUtil.getChatSafeZone());
         x0.set(x0.get()-safeZone);
         x1.set(x1.get()-safeZone);
+    }
+
+    @Inject(method = "handleTagIcon", at = @At(value = "HEAD"), cancellable = true)
+    private void handleTagIcon(CallbackInfo ci) {
+        if (!LegacyOptions.displayChatIndicators.get())
+            ci.cancel();
     }
 }
