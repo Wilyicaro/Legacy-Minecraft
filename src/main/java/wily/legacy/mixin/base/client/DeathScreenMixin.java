fe42041f9764a25bdafbd8a5e0b2d7455cf777fb
Wilyicaro <icarokauanbomfim@hotmail.com>
2026-05-02 11:05:20 -0300

    fix: death screen title

--- src/main/java/wily/legacy/mixin/base/client/DeathScreenMixin.java
+++ src/main/java/wily/legacy/mixin/base/client/DeathScreenMixin.java
@@ -88,7 +88,11 @@ public abstract class DeathScreenMixin extends Screen implements ControlTooltip.
 
     @Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screens/DeathScreen;visitText(Lnet/minecraft/client/gui/ActiveTextCollector;)V", shift = At.Shift.AFTER))
     private void renderLegacyTitle(GuiGraphics guiGraphics, int mouseX, int mouseY, float partialTick, CallbackInfo ci) {
-        LegacyRenderUtil.drawOutlinedString(guiGraphics, this.font, this.title, this.width / 2 - this.font.width(this.title) / 2, this.height / 8 + 10, CommonColor.TITLE_TEXT.get(), CommonColor.TITLE_TEXT_OUTLINE.get(), 0.5f);
+        guiGraphics.pose().pushMatrix();
+        guiGraphics.pose().translate(this.width / 2 - this.font.width(this.title), this.height / 8 + 10);
+        guiGraphics.pose().scale(2.0f);
+        LegacyRenderUtil.drawOutlinedString(guiGraphics, this.font, this.title, 0, 0, CommonColor.TITLE_TEXT.get(), CommonColor.TITLE_TEXT_OUTLINE.get(), 0.5f);
+        guiGraphics.pose().popMatrix();
     }
 
     @WrapOperation(method = "visitText", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/ActiveTextCollector;accept(Lnet/minecraft/client/gui/TextAlignment;IILnet/minecraft/network/chat/Component;)V", ordinal = 0))
