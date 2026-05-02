b6925be306ba6bfc1845bacdde404a2508bf1966
Wilyicaro <icarokauanbomfim@hotmail.com>
2026-05-02 02:55:00 -0300

    fix: duplicate assets in Forge not being handled

--- build-logic/src/main/kotlin/ModPlatformPlugin.kt
+++ build-logic/src/main/kotlin/ModPlatformPlugin.kt
@@ -11,6 +11,7 @@ import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.artifacts.dsl.RepositoryHandler
 import org.gradle.api.artifacts.repositories.MavenArtifactRepository
+import org.gradle.api.file.DuplicatesStrategy
 import org.gradle.api.plugins.JavaPluginExtension
 import org.gradle.api.provider.Property
 import org.gradle.api.publish.PublishingExtension
@@ -146,6 +147,7 @@ abstract class ModPlatformPlugin @Inject constructor() : Plugin<Project> {
 				manifest.attributes(
 					"MixinConfigs" to "${modId}.mixins.json"
 				)
+				duplicatesStrategy = DuplicatesStrategy.EXCLUDE
 			}
 		}
 	}
