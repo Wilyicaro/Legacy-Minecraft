f0554feff7f4b19b94b83cbb40e1c410b9c04227
creepereater201 <258679852+creepereater201@users.noreply.github.com>
2026-05-03 00:00:10 +0100

    fix: rain pushing fog range down

--- src/main/java/wily/legacy/mixin/base/client/AtmosphericFogEnvironmentMixin.java
+++ src/main/java/wily/legacy/mixin/base/client/AtmosphericFogEnvironmentMixin.java
@@ -14,13 +14,13 @@ import wily.legacy.client.LegacyCloudAtmosphere;
 
 @Mixin(AtmosphericFogEnvironment.class)
 public abstract class AtmosphericFogEnvironmentMixin {
-    @Inject(method = "setupFog", at = @At(value = "FIELD", target = "Lnet/minecraft/client/renderer/fog/FogData;environmentalStart:F", ordinal = 0, opcode = Opcodes.PUTFIELD, shift = At.Shift.AFTER))
+    @Inject(method = "setupFog", at = @At(value = "FIELD", target = "Lnet/minecraft/client/renderer/fog/FogData;environmentalStart:F", ordinal = 1, opcode = Opcodes.PUTFIELD, shift = At.Shift.AFTER))
     private void setupFogStart(FogData fogData, Camera camera, ClientLevel clientLevel, float f, DeltaTracker deltaTracker, CallbackInfo ci) {
         if (LegacyOptions.overrideTerrainFogStart.get())
             fogData.environmentalStart = LegacyOptions.getTerrainFogStart() * 16;
     }
 
-    @Inject(method = "setupFog", at = @At(value = "FIELD", target = "Lnet/minecraft/client/renderer/fog/FogData;environmentalEnd:F", ordinal = 0, opcode = Opcodes.PUTFIELD, shift = At.Shift.AFTER))
+    @Inject(method = "setupFog", at = @At(value = "FIELD", target = "Lnet/minecraft/client/renderer/fog/FogData;environmentalEnd:F", ordinal = 1, opcode = Opcodes.PUTFIELD, shift = At.Shift.AFTER))
     private void setupFogEnd(FogData fogData, Camera camera, ClientLevel clientLevel, float f, DeltaTracker deltaTracker, CallbackInfo ci) {
         if (LegacyOptions.overrideTerrainFogEnd.get())
             fogData.environmentalEnd = LegacyOptions.terrainFogEnd.get().floatValue() * 16;
