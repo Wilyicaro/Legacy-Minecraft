dabe22091a248223fed5f5c9f1ab0e70bb963010
creepereater201 <262570857+Creepereater201@users.noreply.github.com>
2026-05-14 10:10:37 +0100

    feat(parity): All tamed mobs now system message on death

--- src/main/java/wily/legacy/mixin/base/LivingEntityMixin.java
+++ src/main/java/wily/legacy/mixin/base/LivingEntityMixin.java
@@ -2,6 +2,7 @@ package wily.legacy.mixin.base;
 
 import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.tags.DamageTypeTags;
 import net.minecraft.tags.FluidTags;
 import net.minecraft.tags.ItemTags;
@@ -11,15 +12,18 @@ import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.OwnableEntity;
+import net.minecraft.world.entity.animal.equine.AbstractHorse;
 import net.minecraft.world.entity.animal.golem.IronGolem;
 import net.minecraft.world.entity.animal.golem.SnowGolem;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.gamerules.GameRules;
 import net.minecraft.world.level.material.Fluid;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.*;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import wily.factoryapi.FactoryAPIClient;
 import wily.factoryapi.base.config.FactoryConfig;
@@ -37,6 +41,16 @@ public abstract class LivingEntityMixin extends Entity {
 
     public LivingEntityMixin(EntityType<?> entityType, Level level) {
         super(entityType, level);
+    }
+
+    @Inject(method = "die", at = @At("HEAD"))
+    private void die(DamageSource damageSource, CallbackInfo ci) {
+        if (!((Object) this instanceof AbstractHorse horse)) return;
+        if (!(level() instanceof ServerLevel level) || !level.getGameRules().get(GameRules.SHOW_DEATH_MESSAGES)) return;
+        LivingEntity owner = horse.getOwner();
+        if (horse.isTamed() && owner instanceof ServerPlayer player) {
+            player.sendSystemMessage(((LivingEntity) (Object) this).getCombatTracker().getDeathMessage());
+        }
     }
 
     @Redirect(method = "travelInAir", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/LivingEntity;onGround()Z", ordinal = 0))
