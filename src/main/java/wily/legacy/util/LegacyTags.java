6d79427b8455353deb6d251c41f0f577ab8c4edd
creepereater201 <258679852+creepereater201@users.noreply.github.com>
2026-05-15 23:25:00 +0100

    feat(parity): splash sound

--- src/main/java/wily/legacy/util/LegacyTags.java
+++ src/main/java/wily/legacy/util/LegacyTags.java
@@ -2,10 +2,12 @@ package wily.legacy.util;
 
 import net.minecraft.core.registries.Registries;
 import net.minecraft.tags.TagKey;
+import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.level.block.Block;
 import wily.legacy.Legacy4J;
 
 public class LegacyTags {
     public static final TagKey<Block> PUSHABLE_BLOCK = TagKey.create(Registries.BLOCK, Legacy4J.createModLocation("pushable"));
     public static final TagKey<Block> WATER_CAULDRONS = TagKey.create(Registries.BLOCK, Legacy4J.createModLocation("water_cauldrons"));
+    public static final TagKey<EntityType<?>> OLD_SPLASH_SOUND = TagKey.create(Registries.ENTITY_TYPE, Legacy4J.createModLocation("old_splash_sound"));
 }
