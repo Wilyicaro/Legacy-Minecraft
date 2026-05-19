336d078e2d06535d4534502809a1682a52e31463
creepereater201 <258679852+creepereater201@users.noreply.github.com>
2026-05-19 23:48:05 +0100

    feat: harvest berry bushes into inventory

---
+++ src/main/java/wily/legacy/mixin/base/SweetBerryBushBlockMixin.java
@@ -0,0 +1,49 @@
+package wily.legacy.mixin.base;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.SweetBerryBushBlock;
+import net.minecraft.world.level.block.VegetationBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.gameevent.GameEvent;
+import net.minecraft.world.level.storage.loot.BuiltInLootTables;
+import net.minecraft.world.phys.BlockHitResult;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(SweetBerryBushBlock.class)
+public abstract class SweetBerryBushBlockMixin extends VegetationBlock {
+    protected SweetBerryBushBlockMixin(Properties properties) {
+        super(properties);
+    }
+
+    @Override
+    protected InteractionResult useWithoutItem(BlockState state, Level level, BlockPos pos, Player player, BlockHitResult hitResult) {
+        if (state.getValue(SweetBerryBushBlock.AGE) <= 1) {
+            return super.useWithoutItem(state, level, pos, player, hitResult);
+        }
+
+        if (level instanceof ServerLevel serverLevel) {
+            Block.dropFromBlockInteractLootTable(
+                    serverLevel,
+                    BuiltInLootTables.HARVEST_SWEET_BERRY_BUSH,
+                    state,
+                    level.getBlockEntity(pos),
+                    null,
+                    player,
+                    (serverlvl, stack) -> player.getInventory().placeItemBackInInventory(stack)
+            );
+            serverLevel.playSound(null, pos, SoundEvents.SWEET_BERRY_BUSH_PICK_BERRIES, SoundSource.BLOCKS, 1.0F, 0.8F + serverLevel.getRandom().nextFloat() * 0.4F);
+            BlockState newState = state.setValue(SweetBerryBushBlock.AGE, 1);
+            serverLevel.setBlock(pos, newState, 2);
+            serverLevel.gameEvent(GameEvent.BLOCK_CHANGE, pos, GameEvent.Context.of(player, newState));
+        }
+
+        return InteractionResult.SUCCESS;
+    }
+}
