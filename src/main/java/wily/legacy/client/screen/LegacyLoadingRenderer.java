76056c3e8c30e32bc39e14bb7b649352dd673e90
creepereater201 <258679852+creepereater201@users.noreply.github.com>
2026-05-28 16:28:08 +0100

    feat: loading bar set sprite

--- src/main/java/wily/legacy/client/screen/LegacyLoadingRenderer.java
+++ src/main/java/wily/legacy/client/screen/LegacyLoadingRenderer.java
@@ -67,13 +67,15 @@ public class LegacyLoadingRenderer implements Renderable {
                         GuiGraphicsExtractor.text(minecraft.font, loadingStage, stageX, stageY, CommonColor.STAGE_TEXT.get());
                     });
                 }
-                try (SpriteContents contents = FactoryGuiGraphics.getSprites().getSprite(LOADING_BACKGROUND).contents()) {
-                    FactoryGuiGraphics.of(GuiGraphicsExtractor).blitSprite(LOADING_BACKGROUND, loadingBarX, loadingBarY, 320, 320 * contents.height() / contents.width());
+                Identifier loadingBackground = accessor.getElementValue("loadingBar.backgroundSprite", LOADING_BACKGROUND, Identifier.class);
+                Identifier loadingBar = accessor.getElementValue("loadingBar.sprite", LOADING_BAR, Identifier.class);
+                try (SpriteContents contents = FactoryGuiGraphics.getSprites().getSprite(loadingBackground).contents()) {
+                    FactoryGuiGraphics.of(GuiGraphicsExtractor).blitSprite(loadingBackground, loadingBarX, loadingBarY, 320, 320 * contents.height() / contents.width());
                 }
                 if (steppedProgress > 0) {
-                    try (SpriteContents contents = FactoryGuiGraphics.getSprites().getSprite(LOADING_BAR).contents()) {
+                    try (SpriteContents contents = FactoryGuiGraphics.getSprites().getSprite(loadingBar).contents()) {
                         int fillWidth = Math.min(318, Math.round(318.0f * steppedProgress / 100.0f));
-                        FactoryGuiGraphics.of(GuiGraphicsExtractor).blitSprite(LOADING_BAR, 318, 318 * contents.height() / contents.width(), 0, 0, loadingBarX + 1, loadingBarY + 1, 0, fillWidth, 318 * contents.height() / contents.width());
+                        FactoryGuiGraphics.of(GuiGraphicsExtractor).blitSprite(loadingBar, 318, 318 * contents.height() / contents.width(), 0, 0, loadingBarX + 1, loadingBarY + 1, 0, fillWidth, 318 * contents.height() / contents.width());
                     }
                 }
                 LegacyTip tip = Legacy4JClient.legacyTipManager.getLoadingTip();
