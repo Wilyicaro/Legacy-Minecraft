95b3b85cd53ffcb8f3836e0ec39326c51721e40c
creepereater201 <258679852+creepereater201@users.noreply.github.com>
2026-05-28 19:48:37 +0100

    feat(tag): full size slot items

--- src/main/java/wily/legacy/util/LegacyTags.java
+++ src/main/java/wily/legacy/util/LegacyTags.java
@@ -2,11 +2,13 @@ package wily.legacy.util;
 
 import net.minecraft.core.registries.Registries;
 import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.level.block.Block;
 import wily.legacy.Legacy4J;
 
 public class LegacyTags {
+    public static final TagKey<Item> FULL_SIZE_SLOT_ITEMS = TagKey.create(Registries.ITEM, Legacy4J.createModLocation("full_size_slot_items"));
     public static final TagKey<Block> PUSHABLE_BLOCK = TagKey.create(Registries.BLOCK, Legacy4J.createModLocation("pushable"));
     public static final TagKey<Block> WATER_CAULDRONS = TagKey.create(Registries.BLOCK, Legacy4J.createModLocation("water_cauldrons"));
     public static final TagKey<Block> SLOW_CHUNK_FEATURES = TagKey.create(Registries.BLOCK, Legacy4J.createModLocation("slow_chunk_features"));
