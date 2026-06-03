d7fa4f9ee0f613b23c026506b96f639cdb170d02
creepereater201 <258679852+creepereater201@users.noreply.github.com>
2026-06-03 15:40:29 +0100

    Revert "feat(parity): zombie pigman also had increased height"
    This reverts commit fcfb85825ef37a90b269d5057fa31b08260d192b.

--- src/main/java/wily/legacy/client/screen/OptionsScreen.java
+++ src/main/java/wily/legacy/client/screen/OptionsScreen.java
@@ -625,7 +625,6 @@ public class OptionsScreen extends PanelVListScreen {
                                 LegacyOptions.legacyFireworks,
                                 LegacyOptions.legacyEvokerFangs,
                                 LegacyOptions.legacyDrownedHeight,
-                                LegacyOptions.legacyZombiePigmanHeight,
                                 LegacyOptions.legacyDrownedAnimation,
                                 LegacyOptions.legacyZombieAggressionAnimation,
                                 LegacyOptions.legacyEntityFireTint,
