6a9202557077487fe10cdc82397118ecd543315a
creepereater201 <258679852+creepereater201@users.noreply.github.com>
2026-06-11 22:07:05 +0100

    feat(parity): downward facing elytra boosting applies VVB

--- src/main/java/wily/legacy/entity/LegacyLocalPlayer.java
+++ src/main/java/wily/legacy/entity/LegacyLocalPlayer.java
@@ -3,4 +3,10 @@ package wily.legacy.entity;
 public interface LegacyLocalPlayer {
 
     boolean canSprintController();
+
+    boolean isLegacyElytraBoosting();
+
+    boolean isLegacyElytraBoostBobbing();
+
+    double getLegacyElytraBoostYBobMovement();
 }
