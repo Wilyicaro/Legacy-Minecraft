03f163b7c600342ff0ff4d67a82327201167ca8b
Wilyicaro <icarokauanbomfim@hotmail.com>
2026-08-16 23:00:28 -0500

    fix: missing import in `LegacyRenderUtil` in Forge and NeoForge

--- src/main/java/wily/legacy/util/client/LegacyRenderUtil.java
+++ src/main/java/wily/legacy/util/client/LegacyRenderUtil.java
@@ -81,6 +81,7 @@ import wily.legacy.util.LegacySprites;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.function.Function;
 import java.util.stream.Collectors;
