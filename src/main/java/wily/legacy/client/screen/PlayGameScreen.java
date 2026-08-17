6033770491d495d5c431e66c59c05691e3b72a2c
creepereater201 <creepereater201@gmail.com>
2026-08-18 00:25:43 +0100

    fix: reuse create world preload

--- src/main/java/wily/legacy/client/screen/PlayGameScreen.java
+++ src/main/java/wily/legacy/client/screen/PlayGameScreen.java
@@ -66,6 +66,7 @@ public class PlayGameScreen extends PanelVListScreen implements ControlTooltip.E
     });
     private final ServerStatusPinger pinger = new ServerStatusPinger();
     private static boolean preloadingCreateWorld;
+    private boolean createWorldPreloaded;
     public boolean isLoading = false;
 
     public static boolean isPreloadingCreateWorld() {
@@ -131,7 +132,10 @@ public class PlayGameScreen extends PanelVListScreen implements ControlTooltip.E
     @Override
     public void added() {
         super.added();
-        preloadCreateWorld(Minecraft.getInstance());
+        if (!createWorldPreloaded) {
+            createWorldPreloaded = true;
+            preloadCreateWorld(Minecraft.getInstance());
+        }
         serverRenderableList.added();
     }
 
