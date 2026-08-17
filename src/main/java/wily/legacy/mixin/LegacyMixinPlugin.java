1c5d28d6698615e8b26704b757e22ad0ecbaba3e
Wilyicaro <icarokauanbomfim@hotmail.com>
2026-08-16 23:28:05 -0500

    chore: leftover methods from `OcclusionCuller09Mixin` handling

--- src/main/java/wily/legacy/mixin/LegacyMixinPlugin.java
+++ src/main/java/wily/legacy/mixin/LegacyMixinPlugin.java
@@ -1,24 +1,16 @@
 package wily.legacy.mixin;
 
 import org.objectweb.asm.tree.ClassNode;
-import org.objectweb.asm.tree.MethodNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
 import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
-import org.spongepowered.asm.service.MixinService;
 import wily.factoryapi.FactoryAPI;
 import wily.legacy.client.LegacyMixinOptions;
 import wily.legacy.config.LegacyMixinToggles;
 
-import java.io.IOException;
 import java.util.List;
 import java.util.Set;
 
 public class LegacyMixinPlugin implements IMixinConfigPlugin {
-    private static final String OCCLUSION_CULLER = "net.caffeinemc.mods.sodium.client.render.chunk.occlusion.OcclusionCuller";
-    private static final String SODIUM_08_DISTANCE_CHECK = "(Lnet/caffeinemc/mods/sodium/client/render/viewport/CameraTransform;Lnet/caffeinemc/mods/sodium/client/render/chunk/RenderSection;F)Z";
-    private static final String SODIUM_09_VISIT_NODE = "(Lnet/caffeinemc/mods/sodium/client/util/collections/WriteQueue;Lnet/caffeinemc/mods/sodium/client/render/chunk/RenderSection;IZZZ)V";
-    private static Boolean sodium08OcclusionCuller;
-    private static Boolean sodium09OcclusionCuller;
 
     @Override
     public void onLoad(String mixinPackage) {
@@ -44,28 +36,6 @@ public class LegacyMixinPlugin implements IMixinConfigPlugin {
 
         boolean hasVivecraft = FactoryAPI.isLoadingMod("vivecraft");
         return !hasVivecraft || !mixinClassName.endsWith("GuiGameRendererMixin");
-    }
-
-    private static boolean hasSodium08OcclusionCuller() {
-        if (sodium08OcclusionCuller == null) sodium08OcclusionCuller = hasOcclusionCullerMethod("isWithinRenderDistance", SODIUM_08_DISTANCE_CHECK);
-        return sodium08OcclusionCuller;
-    }
-
-    private static boolean hasSodium09OcclusionCuller() {
-        if (sodium09OcclusionCuller == null) sodium09OcclusionCuller = hasOcclusionCullerMethod("visitNode", SODIUM_09_VISIT_NODE);
-        return sodium09OcclusionCuller;
-    }
-
-    private static boolean hasOcclusionCullerMethod(String name, String desc) {
-        try {
-            ClassNode node = MixinService.getService().getBytecodeProvider().getClassNode(OCCLUSION_CULLER, false);
-            for (MethodNode method : node.methods) {
-                if (method.name.equals(name) && method.desc.equals(desc)) return true;
-            }
-        } catch (ClassNotFoundException | IOException e) {
-            return false;
-        }
-        return false;
     }
 
     @Override
