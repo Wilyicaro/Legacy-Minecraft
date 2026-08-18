bcdd55cca839b3993ce94eadf599be9f004367fe
creepereater201 <creepereater201@gmail.com>
2026-08-18 17:20:25 +0100

    fix: villager remap

--- src/main/java/wily/legacy/mixin/base/villager_spawn_egg/ResetProfessionMixin.java
+++ src/main/java/wily/legacy/mixin/base/villager_spawn_egg/ResetProfessionMixin.java
@@ -9,7 +9,7 @@ import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(ResetProfession.class)
 public class ResetProfessionMixin {
-    @ModifyExpressionValue(method = "method_47038", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/npc/Villager;getVillagerXp()I"), remap = false)
+    @ModifyExpressionValue(method = "method_47038", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/npc/Villager;getVillagerXp()I"))
     private static int keepSpawnEggProfession(int original, ServerLevel level, Villager villager, long time) {
         return villager.getTags().contains("legacy_spawn_egg_profession") ? 1 : original;
     }
