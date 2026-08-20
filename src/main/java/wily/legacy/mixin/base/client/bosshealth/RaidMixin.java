a848693d64b23223f04acab4cc1266d32fc60887
creepereater201 <creepereater201@gmail.com>
2026-08-20 20:08:19 +0100

    feat(QOL): add legacy boss bar option

--- src/main/java/wily/legacy/mixin/base/client/bosshealth/RaidMixin.java
+++ src/main/java/wily/legacy/mixin/base/client/bosshealth/RaidMixin.java
@@ -1,23 +1,21 @@
 package wily.legacy.mixin.base.client.bosshealth;
 
-import net.minecraft.ChatFormatting;
-import net.minecraft.network.chat.Component;
 import net.minecraft.world.BossEvent;
-import net.minecraft.world.entity.boss.wither.WitherBoss;
+import net.minecraft.world.entity.raid.Raid;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 import wily.legacy.client.LegacyOptions;
 
-@Mixin(WitherBoss.class)
-public class WitherBossMixin {
-    @ModifyArg(method = "<init>", at = @At(value = "INVOKE", target = /*? if >=26.1 {*/"Lnet/minecraft/server/level/ServerBossEvent;<init>(Ljava/util/UUID;Lnet/minecraft/network/chat/Component;Lnet/minecraft/world/BossEvent$BossBarColor;Lnet/minecraft/world/BossEvent$BossBarOverlay;)V"/*?} else {*//*"Lnet/minecraft/server/level/ServerBossEvent;<init>(Lnet/minecraft/network/chat/Component;Lnet/minecraft/world/BossEvent$BossBarColor;Lnet/minecraft/world/BossEvent$BossBarOverlay;)V"*//*?}*/))
-    private Component init(Component component) {
-        return component.copy().withStyle(ChatFormatting.DARK_PURPLE);
-    }
-
+@Mixin(Raid.class)
+public class RaidMixin {
     @ModifyArg(method = "<init>", at = @At(value = "INVOKE", target = /*? if >=26.1 {*/"Lnet/minecraft/server/level/ServerBossEvent;<init>(Ljava/util/UUID;Lnet/minecraft/network/chat/Component;Lnet/minecraft/world/BossEvent$BossBarColor;Lnet/minecraft/world/BossEvent$BossBarOverlay;)V"/*?} else {*//*"Lnet/minecraft/server/level/ServerBossEvent;<init>(Lnet/minecraft/network/chat/Component;Lnet/minecraft/world/BossEvent$BossBarColor;Lnet/minecraft/world/BossEvent$BossBarOverlay;)V"*//*?}*/))
     private BossEvent.BossBarColor initColor(BossEvent.BossBarColor bossBarColor) {
         return LegacyOptions.legacyBossBars.get() ? BossEvent.BossBarColor.PINK : bossBarColor;
+    }
+
+    @ModifyArg(method = "<init>", at = @At(value = "INVOKE", target = /*? if >=26.1 {*/"Lnet/minecraft/server/level/ServerBossEvent;<init>(Ljava/util/UUID;Lnet/minecraft/network/chat/Component;Lnet/minecraft/world/BossEvent$BossBarColor;Lnet/minecraft/world/BossEvent$BossBarOverlay;)V"/*?} else {*//*"Lnet/minecraft/server/level/ServerBossEvent;<init>(Lnet/minecraft/network/chat/Component;Lnet/minecraft/world/BossEvent$BossBarColor;Lnet/minecraft/world/BossEvent$BossBarOverlay;)V"*//*?}*/))
+    private BossEvent.BossBarOverlay initOverlay(BossEvent.BossBarOverlay bossBarOverlay) {
+        return LegacyOptions.legacyBossBars.get() ? BossEvent.BossBarOverlay.PROGRESS : bossBarOverlay;
     }
 }
