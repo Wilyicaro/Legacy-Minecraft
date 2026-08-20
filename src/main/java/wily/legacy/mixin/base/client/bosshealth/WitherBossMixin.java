dd732be46ae12d7705f0d82e5657178329e893ce
creepereater201 <creepereater201@gmail.com>
2026-08-20 20:13:59 +0100

    feat(QOL): add legacy boss bar option

--- src/main/java/wily/legacy/mixin/base/client/bosshealth/WitherBossMixin.java
+++ src/main/java/wily/legacy/mixin/base/client/bosshealth/WitherBossMixin.java
@@ -7,6 +7,7 @@ import net.minecraft.world.entity.boss.wither.WitherBoss;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
+import wily.legacy.client.LegacyOptions;
 
 @Mixin(WitherBoss.class)
 public class WitherBossMixin {
@@ -14,6 +15,6 @@ public class WitherBossMixin {
     private Component init(Component component){
         return component.copy().withStyle(ChatFormatting.DARK_PURPLE);
     }
-    @ModifyArg(method = "<init>",at = @At(value = "INVOKE", target = "Lnet/minecraft/server/level/ServerBossEvent;<init>(Lnet/minecraft/network/chat/Component;Lnet/minecraft/world/BossEvent$BossBarColor;Lnet/minecraft/world/BossEvent$BossBarOverlay;)V"), index = 1)
-    private BossEvent.BossBarColor initColor(BossEvent.BossBarColor bossBarColor){return BossEvent.BossBarColor.PINK;}
+    @ModifyArg(method = "<init>",at = @At(value = "INVOKE", target = "Lnet/minecraft/server/level/ServerBossEvent;<init>(Lnet/minecraft/network/chat/Component;Lnet/minecraft/world/BossEvent$BossBarColor;Lnet/minecraft/world/BossEvent$BossBarOverlay;)V"))
+    private BossEvent.BossBarColor initColor(BossEvent.BossBarColor bossBarColor){return LegacyOptions.legacyBossBars.get() ? BossEvent.BossBarColor.PINK : bossBarColor;}
 }
