6189a4c8c8b00075f047e50581630ce4de105cb8
Wilyicaro <icarokauanbomfim@hotmail.com>
2026-08-21 14:39:59 -0300

    fix: contextual bar not being affected in Forge

--- src/main/java/wily/legacy/mixin/base/client/gui/ExperienceBarRendererMixin.java
+++ src/main/java/wily/legacy/mixin/base/client/gui/ExperienceBarRendererMixin.java
@@ -1,38 +1,27 @@
 package wily.legacy.mixin.base.client.gui;
 
-import net.minecraft.client.gui.Font;
+import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import net.minecraft.client.DeltaTracker;
 import net.minecraft.client.gui.GuiGraphicsExtractor;
-import net.minecraft.client.gui.contextualbar.ContextualBarRenderer;
-import net.minecraft.locale.Language;
-import net.minecraft.network.chat.FormattedText;
+import net.minecraft.client.gui.contextualbar.ExperienceBarRenderer;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import wily.factoryapi.FactoryAPIClient;
-import wily.factoryapi.util.ColorUtil;
-import wily.legacy.client.CommonColor;
-import wily.legacy.client.LegacyOptions;
-import wily.legacy.util.client.LegacyRenderUtil;
+import wily.factoryapi.base.client.UIAccessor;
+import wily.factoryapi.util.FactoryGuiElement;
+import wily.factoryapi.util.FactoryScreenUtil;
 
-@Mixin(ContextualBarRenderer.class)
-public interface ExperienceBarRendererMixin {
-
-    @Inject(method = "extractExperienceLevel", at = @At("HEAD"), cancellable = true)
-    private static void extractExperienceLevel(GuiGraphicsExtractor guiGraphics, Font font, int i, CallbackInfo ci) {
-        ci.cancel();
-
-        if (!LegacyRenderUtil.canDisplayHUD()) return;
-
-        guiGraphics.pose().pushMatrix();
-        guiGraphics.pose().translate(guiGraphics.guiWidth() / 2f, guiGraphics.guiHeight());
-        String exp = String.valueOf(i);
-        int hudScale = LegacyOptions.hudSize.get();
-        boolean hd = LegacyOptions.getUIMode().isHDOrLower();
-        guiGraphics.pose().translate(0, -36f);
-        if (!hd && hudScale != 1) guiGraphics.pose().scale(7 / 8f, 7 / 8f);
-        LegacyRenderUtil.drawOutlinedString(guiGraphics, font, Language.getInstance().getVisualOrder(FormattedText.of(exp)), -font.width(exp) / 2, -2, ColorUtil.withAlpha(CommonColor.EXPERIENCE_TEXT.get(), LegacyRenderUtil.getHUDOpacity()), ColorUtil.withAlpha(0xFF000000, LegacyRenderUtil.getHUDOpacity()), hd && hudScale == 3 || !hd && hudScale == 2 || hudScale == 1 ? 0.51f : 2 / 3f);
-        FactoryAPIClient.getProfiler().pop();
-        guiGraphics.pose().popMatrix();
+@Mixin(ExperienceBarRenderer.class)
+public class ExperienceBarRendererMixin {
+    //? if forge {
+    /*@WrapMethod(method = {"extractBackground", "extractRenderState"})
+    private void extractBackground(GuiGraphicsExtractor graphics, DeltaTracker deltaTracker, Operation<Void> original) {
+        UIAccessor accessor = FactoryScreenUtil.getGuiAccessor();
+        FactoryGuiElement guiElement = FactoryGuiElement.EXPERIENCE_BAR;
+        if (!guiElement.isVisible(accessor)) return;
+        guiElement.prepareRender(graphics, accessor);
+        original.call(graphics, deltaTracker);
+        guiElement.finalizeRender(graphics, accessor);
     }
+    *///?}
+
 }
--- src/main/java/wily/legacy/mixin/base/client/gui/JumpableVehicleBarRendererMixin.java
+++ src/main/java/wily/legacy/mixin/base/client/gui/JumpableVehicleBarRendererMixin.java
@@ -1,38 +1,27 @@
-//? if >=26.1 {
 package wily.legacy.mixin.base.client.gui;
 
 import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import net.minecraft.client.DeltaTracker;
 import net.minecraft.client.gui.GuiGraphicsExtractor;
-import net.minecraft.client.gui.contextualbar.LocatorBarRenderer;
-import net.minecraft.util.ARGB;
+import net.minecraft.client.gui.contextualbar.JumpableVehicleBarRenderer;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
 import wily.factoryapi.base.client.UIAccessor;
 import wily.factoryapi.util.FactoryGuiElement;
 import wily.factoryapi.util.FactoryScreenUtil;
-import wily.legacy.util.client.LegacyRenderUtil;
 
-@Mixin(LocatorBarRenderer.class)
-public class LocatorBarRendererMixin {
-
-    @ModifyArg(method = "lambda$extractRenderState$1", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiGraphicsExtractor;blitSprite(Lcom/mojang/blaze3d/pipeline/RenderPipeline;Lnet/minecraft/resources/Identifier;IIIII)V"), index = 6)
-    private int extractRenderState(int color) {
-        return ARGB.color((int) (ARGB.alpha(color) * LegacyRenderUtil.getHUDOpacity()), ARGB.transparent(color));
-    }
-
+@Mixin(JumpableVehicleBarRenderer.class)
+public class JumpableVehicleBarRendererMixin {
     //? if forge {
     /*@WrapMethod(method = {"extractBackground", "extractRenderState"})
     private void extractBackground(GuiGraphicsExtractor graphics, DeltaTracker deltaTracker, Operation<Void> original) {
         UIAccessor accessor = FactoryScreenUtil.getGuiAccessor();
-        FactoryGuiElement guiElement = FactoryGuiElement.LOCATOR_BAR;
+        FactoryGuiElement guiElement = FactoryGuiElement.JUMP_METER;
         if (!guiElement.isVisible(accessor)) return;
         guiElement.prepareRender(graphics, accessor);
         original.call(graphics, deltaTracker);
         guiElement.finalizeRender(graphics, accessor);
     }
     *///?}
+
 }
-//?}
