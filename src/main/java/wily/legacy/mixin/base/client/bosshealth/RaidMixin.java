29b2c36f00520596d6d3cd7105e9bd5b1b4126f7
creepereater201 <creepereater201@gmail.com>
2026-08-21 14:53:29 +0100

    fix: raid bar color resets after restart

--- src/main/java/wily/legacy/mixin/base/client/bosshealth/RaidMixin.java
+++ src/main/java/wily/legacy/mixin/base/client/bosshealth/RaidMixin.java
@@ -9,12 +9,12 @@ import wily.legacy.client.LegacyOptions;
 
 @Mixin(Raid.class)
 public class RaidMixin {
-    @ModifyArg(method = "<init>", at = @At(value = "INVOKE", target = /*? if >=26.1 {*/"Lnet/minecraft/server/level/ServerBossEvent;<init>(Ljava/util/UUID;Lnet/minecraft/network/chat/Component;Lnet/minecraft/world/BossEvent$BossBarColor;Lnet/minecraft/world/BossEvent$BossBarOverlay;)V"/*?} else {*//*"Lnet/minecraft/server/level/ServerBossEvent;<init>(Lnet/minecraft/network/chat/Component;Lnet/minecraft/world/BossEvent$BossBarColor;Lnet/minecraft/world/BossEvent$BossBarOverlay;)V"*//*?}*/))
+    @ModifyArg(method = "<init>*", at = @At(value = "INVOKE", target = /*? if >=26.1 {*/"Lnet/minecraft/server/level/ServerBossEvent;<init>(Ljava/util/UUID;Lnet/minecraft/network/chat/Component;Lnet/minecraft/world/BossEvent$BossBarColor;Lnet/minecraft/world/BossEvent$BossBarOverlay;)V"/*?} else {*//*"Lnet/minecraft/server/level/ServerBossEvent;<init>(Lnet/minecraft/network/chat/Component;Lnet/minecraft/world/BossEvent$BossBarColor;Lnet/minecraft/world/BossEvent$BossBarOverlay;)V"*//*?}*/))
     private BossEvent.BossBarColor initColor(BossEvent.BossBarColor bossBarColor) {
         return LegacyOptions.legacyBossBars.get() ? BossEvent.BossBarColor.PINK : bossBarColor;
     }
 
-    @ModifyArg(method = "<init>", at = @At(value = "INVOKE", target = /*? if >=26.1 {*/"Lnet/minecraft/server/level/ServerBossEvent;<init>(Ljava/util/UUID;Lnet/minecraft/network/chat/Component;Lnet/minecraft/world/BossEvent$BossBarColor;Lnet/minecraft/world/BossEvent$BossBarOverlay;)V"/*?} else {*//*"Lnet/minecraft/server/level/ServerBossEvent;<init>(Lnet/minecraft/network/chat/Component;Lnet/minecraft/world/BossEvent$BossBarColor;Lnet/minecraft/world/BossEvent$BossBarOverlay;)V"*//*?}*/))
+    @ModifyArg(method = "<init>*", at = @At(value = "INVOKE", target = /*? if >=26.1 {*/"Lnet/minecraft/server/level/ServerBossEvent;<init>(Ljava/util/UUID;Lnet/minecraft/network/chat/Component;Lnet/minecraft/world/BossEvent$BossBarColor;Lnet/minecraft/world/BossEvent$BossBarOverlay;)V"/*?} else {*//*"Lnet/minecraft/server/level/ServerBossEvent;<init>(Lnet/minecraft/network/chat/Component;Lnet/minecraft/world/BossEvent$BossBarColor;Lnet/minecraft/world/BossEvent$BossBarOverlay;)V"*//*?}*/))
     private BossEvent.BossBarOverlay initOverlay(BossEvent.BossBarOverlay bossBarOverlay) {
         return LegacyOptions.legacyBossBars.get() ? BossEvent.BossBarOverlay.PROGRESS : bossBarOverlay;
     }
