f3aece2154e21ce48801c9c492e954d20f36e247
creepereater201 <creepereater201@gmail.com>
2026-08-27 13:34:57 +0100

    fix: villager profession mixins on Forge and NeoForge

--- src/main/java/wily/legacy/mixin/base/villager_spawn_egg/AssignProfessionMixin.java
+++ src/main/java/wily/legacy/mixin/base/villager_spawn_egg/AssignProfessionMixin.java
@@ -15,19 +15,26 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(AssignProfessionFromJobSite.class)
 public class AssignProfessionMixin {
-    //? if <1.21.5 {
+    //? if fabric {
     @ModifyExpressionValue(method = "method_46890", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/npc/VillagerData;getProfession()Lnet/minecraft/world/entity/npc/VillagerProfession;"))
+    //?} else {
+    /*@ModifyExpressionValue(method = "lambda$create$4", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/npc/VillagerData;getProfession()Lnet/minecraft/world/entity/npc/VillagerProfession;"))
+    *///?}
+    //? if <1.21.5 {
     private static VillagerProfession allowSpawnEggProfessionSwap(VillagerProfession original, BehaviorBuilder.Instance<?> instance, MemoryAccessor<?, ?> potentialJobSite, MemoryAccessor<?, ?> jobSite, ServerLevel level, Villager villager, long time) {
         return original != VillagerProfession.NONE && villager.getVillagerXp() == 0 && villager.getTags().contains("legacy_spawn_egg_profession") ? VillagerProfession.NONE : original;
     }
     //?} else {
-    /*@ModifyExpressionValue(method = "method_46890", at = @At(value = "INVOKE", target = "Lnet/minecraft/core/Holder;is(Lnet/minecraft/resources/ResourceKey;)Z"))
-    private static boolean allowSpawnEggProfessionSwap(boolean original, BehaviorBuilder.Instance<?> instance, MemoryAccessor<?, ?> potentialJobSite, MemoryAccessor<?, ?> jobSite, ServerLevel level, Villager villager, long time) {
+    /*private static boolean allowSpawnEggProfessionSwap(boolean original, BehaviorBuilder.Instance<?> instance, MemoryAccessor<?, ?> potentialJobSite, MemoryAccessor<?, ?> jobSite, ServerLevel level, Villager villager, long time) {
         return original || villager.getVillagerXp() == 0 && villager.getTags().contains("legacy_spawn_egg_profession");
     }
     *///?}
 
+    //? if fabric {
     @Inject(method = "method_46891", at = @At("RETURN"))
+    //?} else {
+    /*@Inject(method = "lambda$create$3", at = @At("RETURN"))
+    *///?}
     private static void clearSpawnEggProfessionTag(Villager villager, ServerLevel level, /*? if >=1.21.5 {*//*Holder.Reference<?>*//*?} else {*/VillagerProfession/*?}*/ profession, CallbackInfo ci) {
         villager.playSound(villager.getNotifyTradeSound(), 1.0f, villager.getVoicePitch());
         villager.removeTag("legacy_spawn_egg_profession");
